5e460b9066592184e3f14c7d8c696ef636da4d85
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-11-25 11:45:35 +0100

    problem: multiclass to binary

--- src/env.py
+++ src/env.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess as sb
+from typing import Literal
 
 import cap
 import quapy as qp
@@ -18,5 +19,4 @@ root_dir = os.path.join(cap.env["OUT_DIR"], PROJECT)
 NUM_TEST = 1000
 qp.environ["_R_SEED"] = 0
 
-_valid_problems = ["binary", "multiclass"]
-PROBLEM = "multiclass"
+PROBLEM: Literal["binary", "multiclass"] = "binary"
