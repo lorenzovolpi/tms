5472ce29e60bc91f13806badf3e495bfccccbe09
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2026-01-13 20:50:26 +0100

    refactored tms implementation

--- src/method/base.py
+++ src/method/base.py
@@ -1,4 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+import numpy as np
+from quapy.data import LabelledCollection
+
+from config import ClsVariant, DatasetBundle
 
 
-class ModelSelection(ABC): ...
+class ModelSelection(ABC):
+    def __init__(self, clsf: ClsVariant, D: DatasetBundle):
+        self.clsf = clsf
+        self.D = D
+
+    @abstractmethod
+    def rank(self, acc_fn, val: LabelledCollection, val_posteriors: np.ndarray): ...
+
+    def empty_rank(self):
+        return dict(
+            ranking_vals=[None] * self.D.test_prot.total(),
+            t_train=None,
+            t_test_ave=None,
+        )
--- src/method/tms.py
+++ src/method/tms.py
@@ -1,55 +1,36 @@
-from abc import abstractmethod
 from time import time
 from typing import override
 
 import numpy as np
 import quapy as qp
 from cap.models.cont_table import O_LEAP
-from cap.utils.commons import get_shift
 from quapy.data import LabelledCollection
 from quapy.method.aggregative import KDEyML
 from sklearn.neural_network import MLPClassifier
 
 from config import ClsVariant, DatasetBundle
 from method.base import ModelSelection
-from util import gen_method_df, get_plain_prev
 
 
-class TMS(ModelSelection):
-    def __init__(self, acc_fn, clsf: ClsVariant, D: DatasetBundle):
-        self.acc_fn = acc_fn
-        self.clsf = clsf
-        self.D = D
-
-    @abstractmethod
-    def fit(self, val: LabelledCollection, val_posteriors: np.ndarray): ...
-
-    @abstractmethod
-    def predict(self): ...
+class TMS(ModelSelection): ...
 
 
 class TMS_LEAP(TMS):
     @override
-    def fit(self, val: LabelledCollection, val_posteriors: np.ndarray):
-        self.val = val
-        self.val_posteriors = val_posteriors
+    def rank(self, acc_fn, val: LabelledCollection, val_posteriors: np.ndarray):
+        if self.clsf.ms_ignore:
+            return self.empty_rank()
 
         tinit = time()
-        self.leap = O_LEAP(self.acc_fn, KDEyML(MLPClassifier())).fit(val, val_posteriors)
-        self.t_train = time() - tinit
 
-        return self
+        leap = O_LEAP(acc_fn, KDEyML(MLPClassifier())).fit(val, val_posteriors)
+        ranking_vals = leap.batch_predict(self.D.test_prot, self.D.test_prot_posteriors, get_estim_cts=False)
 
-    @override
-    def predict(self):
-        tinit = time()
-        ranking_vals = self.leap.batch_predict(self.D.test_prot, self.D.test_prot_posteriors, get_estim_cts=False)
-        t_test_ave = (time() - tinit) / self.D.test_prot.total()
+        t_ave = (time() - tinit) / self.D.test_prot.total()
 
         return dict(
             ranking_vals=ranking_vals,
-            t_train=self.t_train,
-            t_test_ave=t_test_ave,
+            t_ave=t_ave,
         )
 
 
@@ -58,54 +39,48 @@ class TMS_RQBS(TMS):
     Reverse Quantification-Based Sampling
     """
 
-    val_q_dict = {}
+    val_sidx_dict = {}
 
     def __init__(
         self,
-        acc_fn,
         clsf: ClsVariant,
         D: DatasetBundle,
         n_vsamples=100,
         sample_size=qp.environ["SAMPLE_SIZE"],
     ):
-        super().__init__(acc_fn, clsf, D)
+        super().__init__(clsf, D)
         self.n_vsamples = n_vsamples
         self.sample_size = sample_size
 
     @override
-    def fit(self, val: LabelledCollection, val_posteriors: np.ndarray):
-        self.val = val
-        self.val_posteriors = val_posteriors
-        q = self.val_sidx_dict.get((self.D.dataset_name, self.D.n_classes), None)
-        if q is None:
+    def rank(self, acc_fn, val: LabelledCollection, val_posteriors: np.ndarray):
+        if self.clsf.ms_ignore:
+            return self.empty_rank()
+
+        tinit = time()
+
+        sidx = self.val_sidx_dict.get((self.D.dataset_name, self.D.n_classes), None)
+        if sidx is None:
             q = KDEyML(MLPClassifier()).fit(val)
-            self.val_q_dict[(self.D.dataset_name, self.D.n_classes)] = q
+            q_hats = [q.quantify(Ui.X) for Ui in self.D.test_prot()]
+            sidx = np.asarray(
+                [[val.sampling_index(self.sample_size, *q_hat) for _ in range(self.n_vsamples)] for q_hat in q_hats]
+            )
+            self.val_sidx_dict[(self.D.dataset_name, self.D.n_classes)] = sidx
 
-        tinit = time()
-        q_hats = [q.quantify(Ui.X) for Ui in self.D.test_prot()]
-        self.sidx = np.asarray(
-            [[val.sampling_index(self.sample_size, *q_hat) for _ in range(self.n_vsamples)] for q_hat in q_hats]
-        )
-        self.t_train = time() - tinit
-
-        return self
-
-    @override
-    def predict(self):
         ranking_vals = []
-        tinit = time()
-        for vidxs in self.sidx:
+        for vidxs in sidx:
             accs = []
             for idx in vidxs:
-                vali_yhat = self.val_posteriors[idx, :].argmax(axis=1)
-                vali_y = self.val[idx, :]
-                accs.append(self.acc_fn(vali_yhat, vali_y))
+                vali_yhat = val_posteriors[idx, :].argmax(axis=1)
+                vali_y = val[idx, :]
+                accs.append(acc_fn(vali_yhat, vali_y))
             ranking_vals.append(np.median(accs))
         ranking_vals = np.array(ranking_vals)
-        t_test_ave = (time() - tinit) / self.D.test_prot.total()
+
+        t_ave = (time() - tinit) / self.D.test_prot.total()
 
         return dict(
             ranking_vals=ranking_vals,
-            t_train=self.t_train,
-            t_test_ave=t_test_ave,
+            t_ave=t_ave,
         )
