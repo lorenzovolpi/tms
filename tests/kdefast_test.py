20accb5406993455995a4b5952ed10dac65ab290
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2026-02-13 01:48:06 +0100

    test updated

--- tests/kdefast_test.py
+++ tests/kdefast_test.py
@@ -4,6 +4,9 @@ from traceback import print_exception
 
 from quapy.method.aggregative import KDEyML
 from quapy.protocol import UPP
+from tqdm import tqdm
+
+from method._kdeyml_cuda import KDEyMLCuda
 
 os.environ["OPENBLAS_NUM_THREADS"] = os.environ["MKL_NUM_THREADS"] = os.environ["OMP_NUM_THREADS"] = str(1)
 
@@ -14,7 +17,12 @@ import pandas as pd
 import quapy as qp
 from cap.error import vanilla_acc
 from cap.models.cont_table import O_LEAP
-from quapy.data.datasets import UCI_BINARY_DATASETS, fetch_UCIBinaryDataset
+from quapy.data.datasets import (
+    UCI_BINARY_DATASETS,
+    UCI_MULTICLASS_DATASETS,
+    fetch_UCIBinaryDataset,
+    fetch_UCIMulticlassDataset,
+)
 from sklearn.neural_network import MLPClassifier
 
 from data import PretainInfo, load_info_paths
@@ -26,35 +34,35 @@ qp.environ["_R_SEED"] = 0
 
 def text():
     ps = [PretainInfo.load(p, fast=True) for p in load_info_paths(domain="text")]
-    pf = [p for p in ps if "yelp_polarity" in p.d_info.name]
+    pf = [p for p in ps if "yahoo" in p.d_info.name]
     for p in pf:
         db = p.load_dataset_bundle()
         h = p.load_pretrained_classifier(db)
 
         db.get_posteriors(h)
 
-        leap = O_LEAP(vanilla_acc, KDEyMLFast(MLPClassifier(), limit_kde_train=1e4))
+        q = KDEyMLCuda()
 
-        tinit = time()
-        leap.fit(db.V, db.V_posteriors)
-        tfit = time() - tinit
+        tqdm.write(f"{p.h_info.name}@{p.d_info.name}")
+        q.fit(*db.V.Xy)
 
-        tinit = time()
-        leap._batch_predict_ct(db.test_prot, db.test_prot_posteriors)
-        ttest = time() - tinit
+        accs = []
+        for Ui in tqdm(db.test_prot(), desc="Testing", total=db.test_prot.total()):
+            prev = q.quantify(Ui.X)
+            accs.append(qp.error.mae(prev, Ui.prevalence()))
 
-        print(f"[{p.h_info.name}@{p.d_info.name}] {tfit=}; {ttest=}")
+        tqdm.write(f"acc={np.mean(accs):.6f}\n")
 
 
 def classic():
     res_bin = defaultdict(list)
-    for d in UCI_BINARY_DATASETS:
-        L, U = fetch_UCIBinaryDataset(d).train_test
+    for d in UCI_MULTICLASS_DATASETS:
+        L, U = fetch_UCIMulticlassDataset(d).train_test
         tinit = time()
         q_orig = KDEyML(MLPClassifier()).fit(L)
         orig_train_t = time() - tinit
         tinit = time()
-        q = KDEyMLFast(MLPClassifier(), limit_kde_train=0.1).fit(L)
+        q = KDEyMLCuda().fit(*L.Xy)
         _train_t = time() - tinit
 
         prot = UPP(U, sample_size=1000, repeats=100, random_state=0, return_type="labelled_collection")
@@ -90,8 +98,8 @@ def classic():
 
     df = pd.DataFrame.from_dict(res_bin)
     pivot = pd.pivot_table(df, index=["dataset"], columns=["q"], values=["acc", "train_t", "test_t"])
-    pivot.to_markdown("output/kdefast_bin.md")
+    pivot.to_markdown("output/kdefast_mul.md")
 
 
 if __name__ == "__main__":
-    classic()
+    text()
